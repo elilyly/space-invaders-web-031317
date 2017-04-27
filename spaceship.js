14535edac8cc750a030ea36f631bfc213cd6bef6
Elizabeth <elizabethnich19@gmail.com>
2017-04-27 18:57:40 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,21 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+
+    // this.crew = crew.forEach(function(member)
+    // {member.currentShip = this}).bind(this)
+    this.crew = crew
+    this.phasers = 5
+    this.shields = 4
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.docked = this.crew.length ? false : true
+    this.phasersCharge = "uncharged"
+
+    if (crew.length > 0){
+      crew.forEach(function(member){
+        member.currentShip = this
+      }.bind(this))
+    }
+  }
+}
